1d7729fec8f8de8a040f6a61ad7378a1d2511244
exdunn <ahdunn@uci.edu>
2015-11-11 00:45:38 -0800

    Test1
    Added two pink guys

---
+++ Assets/takeDamage.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class takeDamage : MonoBehaviour {
+
+	public int health = 1;
+	public Transform shooter;
+	public GameObject explosionPrefab;
+	
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		if (col.GetComponent<projectileDamage>().shooter != this.transform && col.transform.name == "red photon(Clone)") {
+			health--;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (health <= 0)
+		{
+			die();
+		}
+	}
+	
+	void die()
+	{
+		Destroy (gameObject);
+	}
+}
