aff99441c0b88b4d6d15dd695f37afcebda30979
exdunn <ahdunn@uci.edu>
2015-11-12 14:12:01 -0800

    more stuff

--- Assets/Space Blaster/scripts/PickUp.cs
+++ Assets/Space Blaster/scripts/PickUp.cs
@@ -8,8 +8,7 @@ public class PickUp : MonoBehaviour {
 	// Use this for initialization
 	void OnTriggerEnter2D (Collider2D col) 
 	{
-		Debug.Log (col.transform.GetChild(0).name);
-		//GameObject hand = (GameObject) col.transform.GetChild(0);
+		//Debug.Log (col.transform.GetChild(0).name);
 		GameObject gun = (GameObject) Instantiate (weaponPrefab, col.transform.GetChild(0).transform.position, col.transform.rotation);
 		gun.transform.parent = col.transform;
 		Destroy (gameObject);
---
+++ Assets/player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	
+	public int health = 3;
+	public int playerNumber = 1;
+	public Transform shooter;
+	
+	// Use this for initialization
+	void OnTriggerEnter2D(Collider2D col) 
+	{
+		// prevent player from shooting self
+		// yields NullReferenceError when col is a powerup/weapon
+		if (col.GetComponent<projectileDamage>().shooter != this.transform) 
+		{
+			// prevents collision with powerup/weapon from doing damage
+			// incomplete method... think of something more efficient
+			if (col.transform.name == "red photon(Clone)")
+			{
+				health--;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (health <= 0)
+		{
+			die();
+		}
+	}
+	
+	void die()
+	{
+		Destroy (gameObject);
+	}
+}
