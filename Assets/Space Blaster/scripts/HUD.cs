b4ff0a3ef9eab137b09ee35024a9757031abe393
exdunn <ahdunn@uci.edu>
2015-11-15 19:15:02 -0800

    HUD sprites

--- Assets/Space Blaster/scripts/HUD.cs
+++ Assets/Space Blaster/scripts/HUD.cs
@@ -4,13 +4,16 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour {
 
-	public Sprite[] heartSprites;
-	public Image[] heartUI_images;
-	public GameObject[] heartUI;
+	public Sprite[] heartSprites; 
+	public Sprite[] HUD_sprites;
+
 	public player[] player;
 	public Transform[] heartsLocation;
 	public GameObject heartsPrefab;
 	public GameObject parent;
+	
+	GameObject[] heartUI;
+	Image[] heartUI_images;
 
 	void Start()
 	{
@@ -21,13 +24,28 @@ public class HUD : MonoBehaviour {
 		for (int i = 0; i < temp.Length; i++) 
 		{
 			player[i] = temp[i].GetComponent<player>();
-			Debug.Log (player[i].name);
+			//Debug.Log (player[i].name);
+
 			heartUI[i] = (GameObject) Instantiate (heartsPrefab, heartsLocation[i].position, heartsLocation[i].rotation);
 			heartUI[i].transform.parent = parent.transform;
 			heartUI_images[i] = heartUI[i].GetComponent<Image>();
+			heartUI[i].transform.GetChild(0).gameObject.AddComponent<SpriteRenderer>();
+			heartUI[i].transform.GetChild(0).gameObject.GetComponent<SpriteRenderer>().sprite = HUD_sprites[HUDsprite(i)];
 		}
+	}
 
-
+	int HUDsprite(int i)
+	{
+		if (player[i].transform.name == "Green Alien") {
+			return 0;
+		}
+		else if(player[i].transform.name == "Blue Alien"){
+			return 1;
+		}
+		else if (player[i].transform.name == "Pink Alien"){
+			return 2;
+		}
+		return 0;
 	}
 
 	void Update()
--- Assets/Space Blaster/scripts/playerMove.cs
+++ Assets/Space Blaster/scripts/playerMove.cs
@@ -60,7 +60,7 @@ public class playerMove : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D col)
 	{
-		if (col.gameObject.tag == "Ground")
+		if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Player")
 		{
 			isJumping = false;
 			anim.SetBool("jump", false);
