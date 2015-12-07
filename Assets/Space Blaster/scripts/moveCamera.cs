1f55370bee919e82f9d4865b8cd6faf4f7c8dcce
exdunn <ahdunn@uci.edu>
2015-12-06 19:13:15 -0800

    added sc

---
+++ Assets/Space Blaster/scripts/moveCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveCamera : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = new Vector3(transform.position.x + Time.deltaTime, transform.position.y, transform.position.z);
+        if (transform.position.x >= 42)
+        {
+            transform.position = new Vector3(1, transform.position.y, transform.position.z); ;
+        }
+	}
+}
